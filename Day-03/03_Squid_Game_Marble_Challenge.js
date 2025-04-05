6c0cdd356dda2bdfdc82fb58b4546cc624db6b8b
Prathamesh532 <prathameshkalekar0532@gmail.com>
2025-04-05 18:20:22 +0530

    Day 3 - Q3 Squid game marble challenge optimal solution (perfix sum)

--- Day-03/03_Squid_Game_Marble_Challenge.js
+++ Day-03/03_Squid_Game_Marble_Challenge.js
@@ -11,6 +11,22 @@ function processQueries(n, marbles, queries) {
   return result;
 }
 
+function processQueries_(n, marbles, queries) {
+  let preFixSum = new Array(n + 1).fill(0);
+
+  for (let i = 1; i <= n; i++) {
+    preFixSum[i] = preFixSum[i - 1] + marbles[i - 1];
+  }
+
+  let result = [];
+
+  for (let [l, r] of queries) {
+    result.push(preFixSum[r] - preFixSum[l - 1]);
+  }
+
+  return result;
+}
+
 const queries = [
   [1, 4],
   [3, 6],
@@ -18,5 +34,5 @@ const queries = [
 
 const marbles = [10, 20, 30, 40, 50, 60];
 
-const check = processQueries(marbles.length, marbles, queries);
+const check = processQueries_(marbles.length, marbles, queries);
 console.log(check);
